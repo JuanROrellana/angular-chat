14fca534d380320d9c95eeed53ef1925beb1c469
Juan Ramirez <ramirez.orellana.juanjose@gmail.com>
2019-05-10 14:51:17 -0600

    Auth fixes

--- auth.js
+++ auth.js
@@ -6,34 +6,26 @@ exports.authenticate = (email, password) => {
     return new Promise(async (resolve, reject) => {
         try {
             //Get user by Email
-            const user = await User.findOne({
-                email
-            });
+            const user = await User.findOne({ email });
 
             //Decript User
+            // bcrypt.compare(password, user.password, (err, isMatch) => {
+            //     //if (err) throw err;
+            //     if(isMatch){
+            //         resolve(user);
+            //     }else{
+            //         resolve('Autenthication Failed ');
+            //     }
+            // });
+
             bcrypt.compare(password, user.password, (err, res) => {
                 if(res) {
-                    if (res) {
-                        console.log('Exito Es bueno el pass');
                         resolve(user);
-                    }
                 } else {
                     resolve('Error');
                 } 
               });
-
-            // bcrypt.compare(password, user.password, (err, isMatch) => {
-            //     try{
-            //         if (isMatch) {
-            //             console.log('Exito Es bueno el pass');
-            //             resolve(user);
-
-            //         }
-            //     } catch (err){
-            //         reject('Autenthication Failed');
-            //     }
-               
-            // });
+            
         } catch (err) {
             //Email no found
             reject('Autenthication Failed');
